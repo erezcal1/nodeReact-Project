08ed687190db8e6eff6e1702bff7d4bd1bded888
Erez Calderon <erezcal1@gmail.com>
2022-07-25 15:05:45 +0300

    users

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,3 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+let dataArr = [];
+
 // http://localhost:3001/api/user
 router.get("/", (req, res) => {
   res.json({
@@ -7,14 +12,14 @@ router.get("/", (req, res) => {
   });
 });
 
-let dataArr = [];
-
 router.get("/get-all-users", (req, res) => {
   res.json(dataArr);
 });
-// http://localhost:3001/api/add-new-user
+// http://localhost:3001/api/user/add-new-user
 router.post("/add-new-user", (req, res) => {
   console.log(req.body);
   dataArr = [...dataArr, req.body];
   res.json({ msg: "User added successfully" });
 });
+
+module.exports = router;
