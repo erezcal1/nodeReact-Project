bee934cc8e1d92c220ac96bf46eac0a563075ef3
Erez Calderon <erezcal1@gmail.com>
2022-07-27 10:03:44 +0300

    patch and delete

--- routes/api/animals.js
+++ routes/api/animals.js
@@ -8,7 +8,22 @@ router.get("/", (req, res) => {
 });
 // http://localhost:3001/api/animals/add-new-animal
 router.post("/add-new-animal", (req, res) => {
-  animalsArr = [...animalsArr, req.body.animal];
+  animalsArr = [...animalsArr, req.body];
   res.json({ msg: "Animal added successfully" });
+});
+router.patch("/edit-animal/", (req, res) => {
+  let animal = animalsArr.find((item) => item._id === req.body._id);
+  if (animal) {
+    animal.name = req.body.name;
+    res.json({ msg: "Animal edited successfully" });
+  } else res.json({ msg: "Animal not found" });
+});
+
+router.delete("/delete-animal", (req, res) => {
+  let animal = animalsArr.find((item) => item._id === req.body._id);
+  if (animal) {
+    animalsArr = animalsArr.filter((item) => item._id !== req.body._id);
+    res.json({ msg: "Animal deleted successfully" });
+  } else res.json({ msg: "Animal not found" });
 });
 module.exports = router;
